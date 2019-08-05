7dd2c1dff6636cc46d9208c80efe0e79838a02ce
Amy Barba <Amybarba70@gmail.com>
2019-08-05 01:11:28 -0700

    added more styling and images

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {StarwarsCharCard} from "./components/StarwarsCharCard";
+import { StarwarsCharCard } from "./components/StarwarsCharCard";
 import StarWarsCharPic1 from "./images/Luke Skywalker.jpeg";
 import StarWarsCharPic2 from "./images/c3po.png";
 import StarWarsCharPic3 from "./images/r2d2.jpg";
@@ -24,19 +24,14 @@ const App = () => {
     return (
       <div className="App">
         <h1 className="Header">I LOVE Star Wars Characters</h1>
-        
         <div className="Pictures">
-         <img src={StarWarsCharPic1}/>
-         <img src={StarWarsCharPic2}/>
-         <img src={StarWarsCharPic3}/>
-         </div>
-       {starwarsChars.map(charData => {
-         return (
-           <StarwarsCharCard charData={charData} key={charData.name}/>
-           )
-         
-       })}
-
+            <img src={StarWarsCharPic1} alt="Luke Skywalker" />
+            <img src={StarWarsCharPic2} alt="C3PO" />
+            <img src={StarWarsCharPic3} alt="R2D2" />
+        </div>
+        {starwarsChars.map(charData => {
+          return <StarwarsCharCard charData={charData} key={charData.name} />;
+        })}
       </div>
     );
   } else {
